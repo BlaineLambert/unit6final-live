5fb0c23afbba88421759514f91d526f779c73ad0
BlaineLambert <flambert24@basecampcodingacademy.org>
2024-01-25 00:52:44 -0600

    still trying

--- config/settings.py
+++ config/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-3buk(+r^9n$*#t(l&s^$l1*v%ui1vps9n-x2^i#gj2rc#-jss_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['lets-stay-94a4377936e6.herokuapp.com', '127.0.0.1']
 
